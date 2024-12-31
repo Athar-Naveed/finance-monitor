a41f0bdb16e89fb5030d1389885518c43fa3aa2d
Muzammil Ahmed <166981810+azurecoders@users.noreply.github.com>
2024-12-31 20:25:02 +0500

    INITIAL COMMIT

---
+++ src/components/Buttons/button-light.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BtnLight = ({ btnText }: { btnText: string }) => {
+  return (
+    <button className="bg-black border border-neutral-800 text-white px-4 py-2 rounded hover:bg-neutral-800 transition-colors">
+      {btnText}
+    </button>
+  );
+};
+
+export default BtnLight;
---
+++ src/types/global.d.ts
@@ -0,0 +1,18 @@
+// Types
+interface Feature {
+  icon: React.ElementType;
+  title: string;
+  description: string;
+}
+
+interface Step {
+  icon: React.ElementType;
+  title: string;
+  description: string;
+}
+
+interface Stat {
+  icon: React.ElementType;
+  value: string;
+  label: string;
+}
